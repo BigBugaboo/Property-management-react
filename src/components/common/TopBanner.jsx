1dabc9166182db05e76d2f3fe628cb526f6a182b
hejunze <hj497304787@vip.qq.com>
2019-07-03 22:15:54 +0800

    update: 添加登录工具栏。

---
+++ src/components/common/TopBanner.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+import '@/styles/components/common/top-banner.scss';
+
+export default class TopBanner extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogin: true
+        };
+    }
+
+    render() {
+        const { isLogin } = this.state;
+
+        return (
+            <div className='root'>
+                {isLogin ?
+                    <div>
+                        欢迎XXX，进入物业管理系统。
+                        <Button
+                            icon='logout'
+                            shape='round'
+                            type='danger'>
+                            注销
+                        </Button>
+                    </div>
+                    :
+                    <Button
+                        icon='login'
+                        shape='round'
+                        type='primary'>
+                        登录
+                    </Button>
+                }
+            </div>
+        );
+    }
+}

2e180ecd87957c9692cac72913db66c4c53a7ca2
moonjw <1106801543@qq.com>
2019-07-13 21:55:04 +0800

    update: 添加报修和投诉的修改按钮弹出框及判断

--- client/pages/user/UserComplaint.jsx
+++ client/pages/user/UserComplaint.jsx
@@ -1,51 +1,49 @@
 import React, { Component } from 'react';
 import PropType from 'prop-types';
-import { Table, Icon, Input, Button, Collapse, Modal } from 'antd';
+import { Table, Icon, Button } from 'antd';
 
 import '@/styles/pages/user/UserComplaint.scss';
 import Search from '@/components/common/Search';
 import DrawerForm from '@/components/common/DrawerForm';
 
 
-const data = {
-    complaint: [
-        {
-            key: '1',
-            startdate: '2019-6-12',
-            content: 'xxxxxxx',
-            enddate: '',
-            status: '未处理'
-        },
-        {
-            key: '2',
-            startdate: '2019-6-12',
-            content: 'xxxxxxx',
-            enddate: '2019-6-12',
-            status: '已处理'
-        },
-        {
-            key: '3',
-            startdate: '2019-6-12',
-            content: 'xxxxxxx',
-            enddate: '2019-6-12',
-            status: '已处理'
-        },
-        {
-            key: '4',
-            startdate: '2019-6-12',
-            content: 'xxxxxxx',
-            enddate: '2019-6-12',
-            status: '已处理'
-        },
-        {
-            key: '5',
-            startdate: '2019-6-12',
-            content: 'xxxxxxx',
-            enddate: '2019-6-12',
-            status: '已处理'
-        },
-    ],
-};
+const data = [
+    {
+        key: '1',
+        startdate: '2019-6-12',
+        content: 'xxxxxxx',
+        enddate: '',
+        status: '未处理'
+    },
+    {
+        key: '2',
+        startdate: '2019-6-12',
+        content: 'xxxxxxx',
+        enddate: '2019-6-12',
+        status: '已处理'
+    },
+    {
+        key: '3',
+        startdate: '2019-6-12',
+        content: 'xxxxxxx',
+        enddate: '2019-6-12',
+        status: '已处理'
+    },
+    {
+        key: '4',
+        startdate: '2019-6-12',
+        content: 'xxxxxxx',
+        enddate: '2019-6-12',
+        status: '已处理'
+    },
+    {
+        key: '5',
+        startdate: '2019-6-12',
+        content: 'xxxxxxx',
+        enddate: '2019-6-12',
+        status: '已处理'
+    },
+];
 
 
 export class UserComplaint extends Component{
@@ -68,42 +66,6 @@ export class UserComplaint extends Component{
                     name: 'startdate'
                 }
             ],
-            complaint: [
-                {
-                    title: '投诉编号',
-                    dataIndex: 'key',
-                    key: 'key',
-                },
-                {
-                    title: '投诉日期',
-                    dataIndex: 'startdate',
-                    key: 'startdate',
-                },
-                {
-                    title: '投诉内容',
-                    dataIndex: 'content',
-                    key: 'content',
-                },
-                {
-                    title: '处理日期',
-                    dataIndex: 'enddate',
-                    key: 'enddate',
-                },
-                {
-                    title: '状态',
-                    dataIndex: 'status',
-                    key: 'status',
-                },
-                {
-                    title: '操作',
-                    key: 'action',
-                    render: (text, record) => (
-                        <span>
-                            <Button type='primary' className='btn'>修改</Button>
-                        </span>
-                    ),
-                },
-            ],
         };
     }
 
@@ -111,8 +73,12 @@ export class UserComplaint extends Component{
         console.log(e);
     }
 
+    onChange = (e) => {
+        console.log(e);
+    };
+
     render() {
-        const { search, complaint, form } = this.state;
+        const { search, form } = this.state;
         return (
             <div id='complaint'>
                 <div className='search'>
@@ -127,11 +93,38 @@ export class UserComplaint extends Component{
                         btnType='primary'
                         form={form}
                     />
-                    <Table
-                        bordered={true}
-                        columns={complaint}
-                        dataSource={data.complaint}
-                    />
+                    <Table dataSource={data} bordered={true} size='default'>
+                        <Table.Column title='投诉编号' dataIndex='key' key='key' />
+                        <Table.Column title='投诉日期' dataIndex='startdate' key='startdate' />
+                        <Table.Column title='投诉内容' dataIndex='content' key='content' />
+                        <Table.Column title='处理日期' dataIndex='enddate' key='enddate' />
+                        <Table.Column title='状态' dataIndex='status' key='status' />
+                        <Table.Column
+                            title='操作'
+                            key='action'
+                            render={(text, record) => (
+                                <Button.Group>
+                                    { record.enddate == '' &&
+                                    <DrawerForm
+                                        btnText='修改'
+                                        btnIcon='edit'
+                                        btnType='primary'
+                                        onSubmit={this.onChange}
+                                        form={[
+                                            {
+                                                type: 'textArea',
+                                                text: '投诉内容',
+                                                name: 'content',
+                                                placeholder: '请输入你想要投诉的信息',
+                                                value: record.content,
+                                            },
+                                        ]}
+                                    />}
+
+                                </Button.Group>
+                            )}
+                        />
+                    </Table>
                 </div>
             </div>
         );
